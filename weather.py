3a3bba099a7701389a30b94dd56d04045e82e380
laningya <86898999+laningya@users.noreply.github.com>
2022-05-02 15:38:17 +0800

    Delete weather.py

--- weather.py
+++
@@ -1,8 +0,0 @@
-#! /usr/bin/python
-import requests
-
-
-url = 'https://devapi.qweather.com/v7/weather/now?location=101110509&key=bb6d9d9b301c40ac9ac69852710bfc1a'
-response = requests.get(url)
-weather_data = response.json()
-print(weather_data['now'])
