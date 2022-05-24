91401b0d0ebc04e632653c777564d633b95f3c89
mln <610524250@qq.com>
2022-05-24 10:14:46 +0800

    模块化GPIO

--- GPIO/function.c
+++ GPIO/function.c
@@ -1,43 +1,58 @@
 #include <wiringPi.h>
+#include <stdio.h>
 
-int blink(int gpio_port,int dalayms)
+int is_gpio_port(int gpio_port)
 {
-  if((gpio_port  >=0&&gpio_port<=7)||(gpio_port>=21&&gpio_port<=29))
-  {
-    wiringPiSetup();
-    pinMode(gpio_port,OUTPUT);
-    int level = 0;
-    while(1)
-    {
-      level = (level == 0) ? 1 : 0;
-      digitalWrite(gpio_port,level);
-      delay(dalyms);
-    }
-    return 1;
-  }
+  if((gpio_port>=0&&gpio_port<=7)||(gpio_port>=21&&gpio_port<=29))
+      return 1;
   else
-  {
-    printf("Error gpio_port");
-    return 0;
-  }
+      return 0;
 }
-int breathing_light(int gpio_port,int delayms)
+int blink(int gpio_port,int delayms)
 {
-  wiringPiSetup();
-  pinMode(gpio_port,PWM_OUTPUT);
-  int i = 0;
-  while(1)
-  {
-    for(; i < 1024;i++)
+    if(is_gpio_port(gpio_port))
     {
-      pwmWrite(gpio_port,i);
-      delay(delayms);
+        wiringPiSetup();
+        pinMode(gpio_port,OUTPUT);
+        int level = 0;
+        while(1)
+        {
+            level = (level == 0) ? 1 : 0;
+            digitalWrite(gpio_port,level);
+            delay(delayms);
+        }
+        
     }
-    for(;i > -1;i--)
+    else
     {
-      pwmWrite(gpio_port,i);
-      delay(delayms);
+        printf("Error gpio_port\n");
+        return 0;
     }
-  }
-  return 1;
+}
+int hard_breathing_light(int gpio_port,int delayms)
+{
+    if(gpio_port == 1 || gpio_port == 23 || gpio_port == 24 || gpio_port == 26 )
+    {
+        wiringPiSetup();
+        pinMode(gpio_port,PWM_OUTPUT);
+        int i = 0;
+        while(1)
+        {
+            for(; i < 1024;i++)
+            {
+                pwmWrite(gpio_port,i);
+                delay(delayms);
+            }
+            for(;i > -1;i--)
+            {
+                pwmWrite(gpio_port,i);
+                delay(delayms);
+            }
+        }
+    }
+    else
+    {
+        printf("Error gpio_port");
+        return 0;
+    }
 }
