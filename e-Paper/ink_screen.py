cba4788da5b07560e910a95e53eacae5d860c914
mln <610524250@qq.com>
2022-07-06 16:01:44 +0800

    开发墨水屏

--- e-Paper/ink_screen.py
+++ e-Paper/ink_screen.py
@@ -4,6 +4,7 @@ import os
 import epd2in7
 import time
 import requests
+import socket
 from PIL import Image,ImageDraw,ImageFont
 
 
@@ -12,19 +13,19 @@ class Ink_display(object):
         self.epd = epd2in7.EPD()
         self.epd.init()
         self.epd.Clear(0xFF)
-        self.font12 = ImageFont.truetype('./Font.ttc',12)
+        self.font20 = ImageFont.truetype('./Font.ttc',20)
         self.font24 = ImageFont.truetype('./Font.ttc',24)
-        self.font36 = ImageFont.truetype('./Font.ttc',36)
+        self.font80 = ImageFont.truetype('./Font.ttc',80)
 
     def clock(self):
         timeinfo = time.localtime()
         timeymd = time.strftime("%Y-%m-%d",timeinfo)
         timehm = time.strftime("%H:%M",timeinfo)
         times = time.strftime("%S",timeinfo)
-        image = Image.new('1', (self.epd.width, self.epd.height), 255)
+        image = Image.new('1', (self.epd.height,self.epd.width), 255)
         draw = ImageDraw.Draw(image)
-        draw.text((20, 12), timeymd, font = self.font24, fill = 0)
-        draw.text((20, 36), timehm, font = self.font36, fill = 0)
+        draw.text((70, 110), timeymd, font = self.font24, fill = 0)
+        draw.text((35, 30), timehm, font = self.font80, fill = 0)
         self.epd.display(self.epd.getbuffer(image))
         
     def weather(self):
@@ -32,10 +33,10 @@ class Ink_display(object):
         response = requests.get(url)
         weather_data = response.json()
         data = weather_data['now']
-        image = Image.new('1', (self.epd.width, self.epd.height), 255)
+        image = Image.new('1', (self.epd.height,self.epd.width), 255)
         draw = ImageDraw.Draw(image)
-        draw.text((20, 12), '温度:' + data['temp']+ '`C', font = self.font24, fill = 0)
-        draw.text((20, 36), '天气:' + data['text'], font = self.font24, fill = 0)
+        draw.text((20, 12), '温度:' + data['temp']+ '`C', font = self.font20, fill = 0)
+        draw.text((36, 72), '天气:' + data['text'], font = self.font20, fill = 0)
         self.epd.display(self.epd.getbuffer(image))
 
     def raspberry_info(self):
@@ -45,13 +46,23 @@ class Ink_display(object):
             info = tmp.readline()
         RAM_info = info.split()[1:4]
         CPU_use = os.popen("top -n1 | awk '/Cpu\(s\):/ {print $2}'").readline().strip()
-        image = Image.new('1', (self.epd.width, self.epd.height), 255)
+        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        s.connect(('8.8.8.8', 80))
+        IP_local = s.getsockname()[0]
+        IP_public = requests.get('http://ifconfig.me/ip', timeout=1).text.strip()
+        image = Image.new('1', (self.epd.height,self.epd.width), 255)
         draw = ImageDraw.Draw(image)
-        draw.text((20, 12), '温度：'+ CPU_temperature + '`C', font = self.font24, fill = 0)
-        draw.text((20, 36), '内存：'+ RAM_info[2], font = self.font24, fill = 0)
-        draw.text((20, 60), 'CPU：'+ CPU_use + '%', font = self.font24, fill = 0)
+        draw.text((20, 20), '温度：'+ CPU_temperature + '`C', font = self.font20, fill = 0)
+        draw.text((20, 40), '内存：'+ RAM_info[2], font = self.font20, fill = 0)
+        draw.text((20, 60), 'CPU：'+ CPU_use + '%', font = self.font20, fill = 0)
+        draw.text((20, 80), 'IP：'+ IP_local, font = self.font20, fill = 0)
+        draw.text((20, 100), 'IP：'+ IP_public, font = self.font20, fill = 0)
+        self.epd.display(self.epd.getbuffer(image))
+    
+    def image(self):
+        image = Image.open('./sunday.png')
         self.epd.display(self.epd.getbuffer(image))
 
-ink_display = Ink_display()
-ink_display.raspberry_info()
 
+ink_display = Ink_display()
+ink_display.image()
