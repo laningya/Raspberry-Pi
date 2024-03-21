51c9b31e742ec04f107f355057c0d7237da70c30
mln <610524250@qq.com>
2024-03-21 11:02:52 +0800

    自动下载v1.2

--- autoDownload/autoDownload.py
+++ autoDownload/autoDownload.py
@@ -1,104 +1,131 @@
 #!/usr/bin/python3
+import os
+import sys
 import requests
 from urllib.parse import urlparse
 from urllib.parse import quote
 from urllib.parse import unquote
 from bs4 import BeautifulSoup
-import os
-
-# 提取出url中的协议、域名、端口
-def extract_protocol_domain_port(url):
-
-    parsed_url = urlparse(url)
-    protocol = parsed_url.scheme
-    domain = parsed_url.hostname
-    port = parsed_url.port
-    if not port:
-        if protocol == 'http':
-            port = 80
-        elif protocol == 'https':
-            port = 443
-    return protocol, domain, port
 
 
-def extract_links_from_html(html_content):
-    # 使用BeautifulSoup解析HTML内容
-    soup = BeautifulSoup(html_content, 'html.parser')
-    # 查找所有<a>标签
-    a_tags = soup.find_all('a')
-    # 提取超链接
-    links = []
-    for a_tag in a_tags:
-        link = a_tag.get('href')
-        if link:
-            links.append(link)
-    return links
+class AutoDownload:
+    def __init__(self,url):
+            self.url = url
+    # 提取出url中的协议、域名、端口
+    def extract_protocol_domain_port(self):
 
-def get_dir_content():
-    # 获取当前工作目录
-    current_directory = os.getcwd()
+        parsed_url = urlparse(self.url)
+        protocol = parsed_url.scheme
+        domain = parsed_url.hostname
+        port = parsed_url.port
+        if not port:
+            if protocol == 'http':
+                port = 80
+            elif protocol == 'https':
+                port = 443
+        return protocol, domain, port
 
-    # 获取当前目录下的所有文件和文件夹名称
-    files_and_folders = os.listdir(current_directory)
 
-    # 过滤出当前目录下的所有文件名称
-    files = [file for file in files_and_folders if os.path.isfile(os.path.join(current_directory, file))]
+    def extract_links_from_html(self,html_content):
+        # 使用BeautifulSoup解析HTML内容
+        soup = BeautifulSoup(html_content, 'html.parser')
+        # 查找所有<a>标签
+        a_tags = soup.find_all('a')
+        # 提取超链接
+        links = []
+        for a_tag in a_tags:
+            link = a_tag.get('href')
+            if link:
+                links.append(link)
+        return links
 
-    return files
+    def get_dir_content(self):
+        # 获取当前工作目录
+        current_directory = os.getcwd()
 
-def main(url):
+        # 获取当前目录下的所有文件和文件夹名称
+        files_and_folders = os.listdir(current_directory)
 
-    data = requests.get(url)
+        # 过滤出当前目录下的所有文件名称
+        files = [file for file in files_and_folders if os.path.isfile(os.path.join(current_directory, file))]
 
-    if data.status_code != 200:
-        print('请检查url')
-        exit(0)
-    else:
-        #编码当前目录下的文件名
-        files_encoding = []
-        files = get_dir_content()
-        for file in files:
-            files_encoding.append(quote(file, encoding='utf-8'))
-        #获取协议、域名、端口
-        protocol, domain, port = extract_protocol_domain_port(url)
-        #print(protocol, domain, port)
-        url = protocol + '://' + domain +':'+ str(port)
-        #print(url)
+        return files
+    def get_dirs(self):
 
-        href_contents = extract_links_from_html(data.text)
-        #print(href_contents)
+        folders = [folder for folder in os.listdir() if os.path.isdir(folder)]
+        return folders
 
-        #去除顶部的超链接
-        flag = href_contents.index('..')
-        href_contents = href_contents[flag + 1:]
+    def get_dir_name(self,href_content):
 
-        #去除重复的文件内容
-        for file_encoding in files_encoding:
-            for href_content in href_contents:
-                if file_encoding in href_content:
-                    href_contents.remove(href_content)
-                    break
+        last_occurrence = href_content.rfind('/')
+        second_last_occurrence = href_content.rfind('/', 0, last_occurrence)
+        dir_name = href_content[second_last_occurrence + 1:last_occurrence]
+        return unquote(dir_name, encoding='utf-8')
 
-        #下载文件
-        for href_content in href_contents:
-            _url = url + href_content
-            #进行目录下载
-            if href_content[-1] == '/':
-                last_occurrence = href_content.rfind('/')
-                second_last_occurrence = href_content.rfind('/', 0, last_occurrence)
-                dir_name = href_content[second_last_occurrence + 1:last_occurrence]
-                dir_name = unquote(dir_name,encoding='utf-8')
-                #print(dir_name)
-                current_dir = os.getcwd()
-                os.system('mkdir ' + dir_name)
-                os.chdir(current_dir + '/' + dir_name)
-                print(os.getcwd())
-                main(_url)
-                os.chdir(current_dir)
-                continue
+    def main(self):
+
+        if self.url[-1] != '/':
+            os.system('wget ' + self.url)
+        else:
+            data = requests.get(self.url)
+
+            if data.status_code != 200:
+                print('请检查url')
+                exit(0)
             else:
-                os.system('wget ' + _url)
+                #检查文件夹是否存在
+                dir_name = self.get_dir_name(self.url)
+                folders = self.get_dirs()
+                current_dir = os.getcwd()
+                if dir_name not in folders:
+                    os.system('mkdir ' + dir_name)
+                os.chdir(current_dir + '/' + dir_name)
 
-print('请输入url：')
-url = input()
-main(url)
+                #编码目录下的文件名
+                files_encoding = []
+                files = self.get_dir_content()
+                for file in files:
+                    files_encoding.append(quote(file, encoding='utf-8'))
+                #获取协议、域名、端口
+                protocol, domain, port = self.extract_protocol_domain_port()
+                #print(protocol, domain, port)
+                url = protocol + '://' + domain +':'+ str(port)
+                #print(url)
+
+                href_contents = self.extract_links_from_html(data.text)
+                #print(href_contents)
+
+                #去除顶部的超链接
+                flag = href_contents.index('..')
+                href_contents = href_contents[flag + 1:]
+
+                #去除重复的文件内容
+                for file_encoding in files_encoding:
+                    for href_content in href_contents:
+                        if file_encoding in href_content:
+                            href_contents.remove(href_content)
+                            break
+
+                #下载文件
+                for href_content in href_contents:
+                    _url = url + href_content
+                    #进行目录下载
+                    if href_content[-1] == '/':
+                        self.url = _url
+                        self.main()
+                        continue
+                    else:
+                        os.system('wget ' + _url)
+                os.chdir(current_dir)
+
+if __name__ == '__main__':
+    arguments = sys.argv
+    len_cmd = len(arguments)
+    if len_cmd == 3:
+        if arguments[1] == '-l':
+            url = arguments[2]
+    if len_cmd == 1:
+        print('请输入url:',end = '')
+        url =input()
+    autoDownload = AutoDownload(url)
+    autoDownload.main()
